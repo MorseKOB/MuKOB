1e155992ade0ec177ee8f10d3e2d4d0322f18770
AESilky <aesilky@gmail.com>
2023-02-05 23:21:13 -0800

    Remove build from repo.

--- build/CMakeFiles/MuKOB.dir/lib/sd_card/sd_driver/crc.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/MuKOB.dir/lib/sd_card/sd_driver/crc.c.obj: \
- /home/ed/code/mukob/MuKOB/src/lib/sd_card/sd_driver/crc.c \
- /home/ed/code/mukob/MuKOB/src/lib/sd_card/sd_driver/crc.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h
--- build/ui/rotory_encoder/CMakeFiles/rotory_encoder_quadrature_encoder_pio_h.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for rotory_encoder_quadrature_encoder_pio_h.
